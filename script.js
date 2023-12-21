8d57c058be1b7071b36a4e1fb966212998f9b271
mazumder123 <112567802+mazumder123@users.noreply.github.com>
2023-12-22 02:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) {
+
   //   write your code here
+	function hasKey(key) {
+  // Check if the key exists in the global object sampleObject
+  return sampleObject.hasOwnProperty(key);
 }
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
